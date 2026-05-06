628e2a0600af5720df29d5f6aad2d54efbc467a6
Eduardo Nishimura <edunish@gmail.com>
2026-05-06 02:35:52 -0300

    feat: download button

--- client/src/chart/chart.tsx
+++ client/src/chart/chart.tsx
@@ -4,6 +4,7 @@ import getTopArtists from "../hooks/get-lastfm";
 import { parseAlbums } from "../components/parser-albums";
 import { AlbumTreemap } from "../components/treemap";
 import { useNavigate } from "react-router-dom";
+import { toPng } from "html-to-image";
 
 export default function Chart() {
     const { formData } = useFormContext();
@@ -26,9 +27,26 @@ export default function Chart() {
 
     const albums = parseAlbums(data?.topalbums?.album || []);
 
+    const handleDownload = () => {
+        const node = document.getElementById("chart");
+        if (node) {
+            toPng(node)
+                .then((dataUrl) => {
+                    const link = document.createElement("a");
+                    link.download = "treemap.png";
+                    link.href = dataUrl;
+                    link.click();
+                })
+                .catch((err) => {
+                    console.error("Error generating image:", err);
+                });
+        }
+    };
+
     return (
         <>
             <AlbumTreemap albums={albums} />
+            <button className="w-1/3 rounded-2xl h-12 bg-blue-500 text-white hover:bg-blue-700 transition-colors cursor-pointer" onClick={handleDownload}>Download</button>
         </>
     );
 }

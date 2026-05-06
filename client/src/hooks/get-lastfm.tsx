44762138971018df7fead82a61f5295cc14361ed
Eduardo Nishimura <edunish@gmail.com>
2026-05-06 02:47:38 -0300

    fix: adding https req to last fm api

--- client/src/hooks/get-lastfm.tsx
+++ client/src/hooks/get-lastfm.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default async function getTopArtists(username: string, period: string, chartType: string) {
     const LASTFM_API_KEY = "7a260eeecf6729213ec7ba3881a7f745";
-    const URL = `http://ws.audioscrobbler.com/2.0/?method=user.gettop${chartType}&user=${username}&period=${period}&format=json&api_key=${LASTFM_API_KEY}`;
+    const URL = `https://ws.audioscrobbler.com/2.0/?method=user.gettop${chartType}&user=${username}&period=${period}&format=json&api_key=${LASTFM_API_KEY}`;
 
     try {
         const response = await axios.get(URL);
